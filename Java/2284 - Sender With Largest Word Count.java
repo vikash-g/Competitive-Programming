8c0106e476fd2d09103a017827de6f54960a128b
Vikash Gupta <vikashgupta.1908@gmail.com>
2022-05-28 22:24:01 +0530

    Add 2284 - Sender With Largest Word Count Solution

---
+++ Java/2284 - Sender With Largest Word Count.java
@@ -0,0 +1,27 @@
+/**
+ * @URL: https://leetcode.com/problems/sender-with-largest-word-count/
+ */
+
+// Time : O(n)
+// Space: O(n)
+class Solution {
+    public String largestWordCount(String[] messages, String[] senders) {
+        Map<String, Integer> map = new HashMap<>();
+        int n = messages.length;
+        int maxWordCount = 0;
+        String maxWordSender = null;
+
+        for (int i = 0; i < n; ++i) {
+            int wordCount = map.getOrDefault(senders[i], 0) + messages[i].split(" ").length;
+            map.put(senders[i], wordCount);
+
+            if (wordCount > maxWordCount) {
+                maxWordCount = wordCount;
+                maxWordSender = senders[i];
+            } else if (wordCount == maxWordCount && senders[i].compareTo(maxWordSender) > 0) {
+                maxWordSender = senders[i];
+            }
+        }
+        return maxWordSender;
+    }
+}
--- Solutions2001+.md
+++ Solutions2001+.md
@@ -9,6 +9,7 @@
 |#|Title|Solution|Time|Space|Difficulty|Tags|Note|
 |:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|
 |2283|[Check if Number Has Equal Digit Count and Digit Value](https://leetcode.com/problems/check-if-number-has-equal-digit-count-and-digit-value/)|[Java](./Java/2283%20-%20Check%20if%20Number%20Has%20Equal%20Digit%20Count%20and%20Digit%20Value.java)|_O(n)_|_O(1)_|Easy|String, HashMap||
+|2284|[Sender With Largest Word Count](https://leetcode.com/problems/sender-with-largest-word-count/)|[Java](./Java/2284%20-%20Sender%20With%20Largest%20Word%20Count.java)|_O(n)_|_O(n)_|Medium|String, Array, HashMap||
 
 <br/>
 <div align="right">
